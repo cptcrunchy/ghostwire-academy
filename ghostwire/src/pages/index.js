4a25d31427569316476f3c8e2cf4469cbc218f80
Cpt_Crunch <jasong84@gmail.com>
2019-08-10 14:13:18 -0500

    Created standard color palette and making components

--- ghostwire/src/pages/index.js
+++ ghostwire/src/pages/index.js
@@ -10,6 +10,7 @@ import Sidebar from '../components/Sidebar';
 import Scroll from '../components/Scroll';
 import Form from '../components/Form';
 import SocialMedia from '../components/SocialMedia';
+import DescriptionSection from '../components/ProgramDescription';
 import ContactEmail from '../components/ContactEmail';
 
 
@@ -104,76 +105,9 @@ const IndexPage = () => (
         </section>
       </section>
 
-      <section id="two" className="wrapper style3 fade-up">
-        <div className="inner">
-          <h2>What we do</h2>
-          <p>
-            Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-            turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
-            lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
-            imperdiet est velit quis lorem.
-          </p>
-          <div className="features">
-            <section>
-              <span className="icon major fa-code" />
-              <h3>Lorem ipsum amet</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-lock" />
-              <h3>Aliquam sed nullam</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-cog" />
-              <h3>Sed erat ullam corper</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-desktop" />
-              <h3>Veroeros quis lorem</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-chain" />
-              <h3>Urna quis bibendum</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-diamond" />
-              <h3>Aliquam urna dapibus</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-          </div>
-          <ul className="actions">
-            <li>
-              <Link className="button" to="/generic">
-                Learn more
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <DescriptionSection />
 
-      <section id="three" className="wrapper style4-alt fade-up">
+      <section id="three" className="wrapper style4 fade-up">
         <div className="inner">
           <h2>Get in touch</h2>
           <p>
