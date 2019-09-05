10aec9956610cef36692f628248b454d9a56ca64
Cpt_Crunch <jasong84@gmail.com>
2019-09-04 22:54:48 -0500

    removed country from Address Component

--- ghostwire/src/components/AddressComponent.js
+++ ghostwire/src/components/AddressComponent.js
@@ -4,7 +4,7 @@ import '../assets/sass/main.scss';
 const AddressComponent = () => (
 <li>
     <h3>Address</h3>
-    <span>222 Any St<br />San Antonio, TX<br />USA</span>
+    <span>222 Any St<br />San Antonio, TX</span>
 </li>
 );
 
