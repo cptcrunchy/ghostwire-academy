e60a18f018475da1659ed1cb3aa3d5d6b13ac562
Cpt_Crunch <jasong84@gmail.com>
2019-09-17 22:54:10 -0500

    changed case for form method

--- ghostwire-website/src/components/Form.js
+++ ghostwire-website/src/components/Form.js
@@ -3,7 +3,7 @@ import '../assets/sass/main.scss';
 
 const Form = () => (
     <section>
-        <form method="post" name="enrollmentForm" data-netlify="true" action="/generic">
+        <form method="POST" name="enrollmentForm" data-netlify="true" action="/generic">
             <div className="fields">
                 <div className="field half">
                     <label htmlFor="name">Parent / Guardian Name</label>
